dc2e6f966b65d885b064f59d432c60ccbe1a12ca
Narglc Yu <qingcheliuzhi@qq.com>
2024-02-22 22:42:04 +0800

    定时任务

--- main.go
+++ main.go
@@ -5,7 +5,12 @@ import (
 	"os"
 	"time"
 
+	"gopkg.in/telebot.v3"
 	tele "gopkg.in/telebot.v3"
+)
+
+const (
+	notificationTime = "09:00"
 )
 
 func main() {
@@ -45,5 +50,27 @@ func main() {
 		return c.Send("反弹")
 	})
 
+	go sendDailyNotifications(b)
+
 	b.Start()
+}
+
+func sendDailyNotifications(bot *telebot.Bot) {
+	for {
+		now := time.Now().Format("15:04")
+		if now == notificationTime {
+			// 发送每日通知消息给所有用户
+			users := []string{"m4kvkg1x0gucjhcceikj"} // 在这里添加你想要通知的用户的 ID
+			for _, userID := range users {
+				msg := "这是每日通知！"
+				_, err := bot.Send(telebot.ChatID(userID), msg)
+				if err != nil {
+					log.Printf("无法发送消息给用户 %s: %v", userID, err)
+				}
+			}
+			time.Sleep(time.Minute) // 避免重复发送通知
+		} else {
+			time.Sleep(30 * time.Second) // 每 30 秒检查一次当前时间
+		}
+	}
 }
